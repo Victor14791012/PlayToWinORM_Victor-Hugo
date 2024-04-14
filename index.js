c459b75437b16d2579c5acbc51bcd8347262b9d9
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-04-14 16:54:03 -0300

    video aula moodle

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const conn = require("./db/conn");
 
-conn.authenticate().then( () => {
-    console.log("Conectado ao banco de dados com sucesso!");
-}).catch( (err) => {
-    console.log("Ocorreu um erro: " + err);
-})
+const Usuario = require("./models/Usuario");
+
+conn.sync().then(() => {
+    console.log("Banco de dados conectado e estruturado sincronizada!");
+}).catch((err) => {
+    console.log("Erro ao conectar")
+});
+
+
+//conn.authenticate().then( () => {
+  //  console.log("Conectado ao banco de dados com sucesso!");
+//}).catch( (err) => {
+  //  console.log("Ocorreu um erro: " + err);
+//})
     
    
