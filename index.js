99267c7ba2aa5d2fa41a037efb2777a5d4d66754
Victor14791012 <victor.carvalhok23@gmail.com>
2024-04-15 14:00:32 -0400

    update
    terminado o código seguindo a vídeo aula agora falta a da recuperação

--- index.js
+++ index.js
@@ -1,48 +1,43 @@
 require("dotenv").config();
 const conn = require("./db/conn");
-
 const Usuario = require("./models/Usuario");
-const express= require("express");
+const express = require("express");
 const app = express();
-app.use(
-  express.urlencoded({
-    extended:true,
-  })
-);
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get("/usuarios/novo",(req, res)=>{
-  res.sendFile('${__dirname}'/views/formUsuario.html)
+
+app.get("/usuarios/novo", (req, res) => {
+  res.sendFile(`${__dirname}/views/formUsuario.html`);
 });
 
-app.post("/usuarios/novo",(req,res)=>{
+app.post("/usuarios/novo", async (req, res) => {
   const nickname = req.body.nickname;
-  const nome= req.body.nome;
+  const nome = req.body.nome;
 
   const dadosUsuario = {
     nickname,
     nome,
   };
-});
 
-const usuario = await Usuario.create(dadosUsuario);
-res.send("Usuário inserido sob o id"+usuario.id);
-
-app.listen(8000,()=> {
-  console.log("rodando")
-});
-
-conn.sync().then(() => {
-    console.log("Banco de dados conectado e estruturado sincronizada!");
-}).catch((err) => {
-    console.log("Erro ao conectar")
+  try {
+    const usuario = await Usuario.create(dadosUsuario);
+    res.send(`<script>alert("Usuário inserido sob o id ${usuario.id}"); window.location.href = '/usuarios/novo';</script>`);
+  } catch (error) {
+    console.error("Erro ao criar usuário:", error);
+    res.send(`<script>alert("Erro ao criar usuário: ${error.message}"); window.location.href = '/usuarios/novo';</script>`);
+  }
 });
 
 
-//conn.authenticate().then( () => {
-  //  console.log("Conectado ao banco de dados com sucesso!");
-//}).catch( (err) => {
-  //  console.log("Ocorreu um erro: " + err);
-//})
-    
-   
+app.listen(8000, () => {
+  console.log("Rodando na porta 8000");
+});
+
+conn.sync()
+  .then(() => {
+    console.log("Banco de dados conectado e estrutura sincronizada!");
+  })
+  .catch((err) => {
+    console.error("Erro ao conectar ao banco de dados:", err);
+  });
