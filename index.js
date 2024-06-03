41568d43933dae78e59e45fe4736c3f83cd68345
Victor Carvalho <victor.carvalhok23@gmail.com>
2024-06-03 16:07:40 -0400

    atualização
    delete funcionando

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const conn = require("./db/conn");
 const Usuario = require("./models/Usuario");
-const Jogo = require("./models/Jogo"); 
+const Jogo = require("./models/Jogo");
 const express = require("express");
-const exphbs= require("express-handlebars");
+const exphbs = require("express-handlebars");
 
 const app = express();
-app.engine("handlebars",exphbs.engine());
-app.set("view engine","handlebars");
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,11 +17,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/usuarios", async (req, res) => {
-  const usuarios = await Usuario.findAll({raw: true}); 
-  res.render("usuarios" , {usuarios});
+  const usuarios = await Usuario.findAll({ raw: true });
+  res.render("usuarios", { usuarios });
 });
-
-
 
 app.get("/usuarios/novo", (req, res) => {
   res.render("formUsuario");
@@ -31,10 +29,7 @@ app.get("/usuarios/:id/update", async (req, res) => {
   const id = parseInt(req.params.id);
   const usuario = await Usuario.findByPk(id, { raw: true });
   res.render("formUsuario", { usuario });
-
-
 });
-
 
 app.post("/usuarios/:id/update", async (req, res) => {
   const id = parseInt(req.params.id);
@@ -49,30 +44,26 @@ app.post("/usuarios/:id/update", async (req, res) => {
   }
 });
 
-app.post("/usuarios/:id/delete", async (req, res) => {
-  
+app.get("/usuarios/:id/delete", async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  try {
+    await Usuario.destroy({ where: { id } });
+    res.send(`<script>alert("Usuário deletado com sucesso"); window.location.href = '/usuarios';</script>`);
+  } catch (error) {
+    console.error("Erro ao deletar usuário:", error);
+    res.send(`<script>alert("Erro ao deletar usuário: ${error.message}"); window.location.href = '/usuarios';</script>`);
+  }
 });
-
-
-
-
 app.get("/jogos/novo", (req, res) => {
   res.sendFile(`${__dirname}/views/formJogo.html`);
 });
 
-
-
 app.post("/usuarios/novo", async (req, res) => {
-  const nickname = req.body.nickname;
-  const nome = req.body.nome;
-
-  const dadosUsuario = {
-    nickname,
-    nome,
-  };
+  const { nickname, nome } = req.body;
 
   try {
-    const usuario = await Usuario.create(dadosUsuario);
+    const usuario = await Usuario.create({ nickname, nome });
     res.send(`<script>alert("Usuário inserido sob o id ${usuario.id}"); window.location.href = '/usuarios/novo';</script>`);
   } catch (error) {
     console.error("Erro ao criar usuário:", error);
@@ -80,25 +71,17 @@ app.post("/usuarios/novo", async (req, res) => {
   }
 });
 
-
-
-
 app.post("/jogos/novo", async (req, res) => {
   const { titulo, descricao, precoBase } = req.body;
 
   try {
-    const jogo = await Jogo.create({
-      titulo,
-      descricao,
-      precoBase,
-    });
+    const jogo = await Jogo.create({ titulo, descricao, precoBase });
     res.send(`<script>alert("Jogo inserido sob o id ${jogo.id}"); window.location.href = '/jogos/novo';</script>`);
   } catch (error) {
     console.error("Erro ao criar jogo:", error);
     res.send(`<script>alert("Erro ao criar jogo: ${error.message}"); window.location.href = '/jogos/novo';</script>`);
   }
 });
-
 
 app.listen(8000, () => {
   console.log("Rodando na porta 8000");
